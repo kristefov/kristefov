65e53fda09565ab91e9770dd72b371e5f8aa0f5b
kristiyan <kristiyan.tefov@icloud.com>
2023-06-25 22:36:40 +0100

    navbar hide effect

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { links } from "../data";
 import "./navbar.css"
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="nav">
-      <div className="nav__menu">
+      <div className={`${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`}>
         <ul className="nav__list">
           {links.map(({ name, icon, path }, index) => {
             return (
               <li className="nav__item" key={index}>
-                <NavLink to={path} className={({isActive}) => isActive ? 'nav__link active-nav' : 'nav__link'}>
+                <NavLink to={path} className={({isActive}) => isActive ? 'nav__link active-nav' : 'nav__link'}
+                onClick={() => setShowMenu(!showMenu)}>
                   {icon}
                   <h3 className="nav__name">{name}</h3>
                 </NavLink>
@@ -22,7 +24,7 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div className="nav__toogle">
+      <div className={`${showMenu ? 'nav__toggle animate-toggle' : 'nav__toggle'}`} onClick={() => setShowMenu(!showMenu)}>
         <span></span>
         <span></span>
         <span></span>
