8094e4f074ef465c8fa28701feda793f1e054d72
kristiyan <kristiyan.tefov@icloud.com>
2023-06-26 02:09:59 +0100

    home section styling

--- Portfolio/src/pages/home/Home.jsx
+++ Portfolio/src/pages/home/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Profile from "../../assets/home.jpg";
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import "./home.css";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home section grid">
+      <img src={Profile} alt="" className="home__img" />
+      <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>I'm Kristiyan Tefov.</span> Full Stack Web Developer
+          </h1>
+          <p className="home__description">
+            I am a versatile full stack web developer skilled in both front-end
+            and back-end development, capable of creating and maintaining robust
+            web applications from start to finish.
+          </p>
+          <Link to="/about" className="button">
+            More About Me{" "}
+            <span className="button__icon">
+              <FaArrowRight />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
