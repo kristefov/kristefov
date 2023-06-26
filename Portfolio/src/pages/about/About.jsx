bfaff5bc9878b8a8af82f64127402c74727fddf5
kristiyan <kristiyan.tefov@icloud.com>
2023-06-26 14:44:31 +0100

    Download CV Completed

--- Portfolio/src/pages/about/About.jsx
+++ Portfolio/src/pages/about/About.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Info from '../../components/Info'
+import Stats from '../../components/Stats'
+import { FaDownload } from 'react-icons/fa'
+import CV from '../../assets/CV Kristiyan Tefov.pdf'
 
 const About = () => {
   return (
@@ -8,6 +12,12 @@ const About = () => {
         <div className="about__container grid">
           <div className="about__info">
             <h3 className="section subtitle">Personal Infos</h3>
+            <ul className="info__list grid">
+              <Info />
+            </ul>
+            <a href={CV} download='' className="button">
+              Download CV<span className="button__icon"><FaDownload /></span>
+            </a>
           </div>
         </div>
       </section>
